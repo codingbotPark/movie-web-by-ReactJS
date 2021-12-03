7fab5293b9b9fc9c65132814b4939ab0e077ffad
박병관 <85085375+codingbotPark@users.noreply.github.com>
2021-12-03 20:44:07 +0900

    12.3 router

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -1,6 +1,25 @@
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  // switch는 router를 찾는 역할을 한다
+  return <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path = "/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
--- movie-web/src/components/Movie.js
+++ movie-web/src/components/Movie.js
@@ -1,10 +1,15 @@
 // 어떤 props를 가지고 있는지 판단하기 위해 proptypes를 사용
 import PropTypes from "prop-types" 
-function Movie({coverImg,title,summary,genres}){
+// link를 사용하기 위해 import
+import {Link} from "react-router-dom"
+
+function Movie({id,coverImg,title,summary,genres}){
     return (
     <div>
     <img src = {coverImg} alt = {title} />
-    <h2>{title}</h2>
+    <h2>
+      <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
     <p>{summary}</p>
     <ul>
       {genres.map(g => <li>{g}</li>)}
@@ -14,6 +19,7 @@ function Movie({coverImg,title,summary,genres}){
 }
 
 Movie.propTypes = {
+  id:PropTypes.number.isRequired,
     coverImg : PropTypes.string.isRequired,
     title:PropTypes.string.isRequired,
     summary:PropTypes.string.isRequired,
